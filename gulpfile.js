90815a6a18d1ac6bab2fff6ef07cbcf5ea4b0fbf
Alexandra Godun <mister.blblbl@gmail.com>
2016-05-10 14:28:21 +0200

    Автоматическая сборка проекта

--- gulpfile.js
+++ gulpfile.js
@@ -5,10 +5,18 @@ var sass = require("gulp-sass");
 var plumber = require("gulp-plumber");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
+var mqpacker = require("css-mqpacker");
+var minify = require("gulp-csso");
+var rename = require("gulp-rename");
+var imagemin = require("gulp-imagemin");
 var server = require("browser-sync");
+var clean = require("gulp-clean");
+var runSequence = require('run-sequence');
 
+//Собрать стилевой файл, добавить префиксы,
+// скомбинировать медиа-вырадения, минифицировать файл
 gulp.task("style", function() {
-  gulp.src("sass/style.scss")
+  return gulp.src("sass/style.scss")
     .pipe(plumber())
     .pipe(sass())
     .pipe(postcss([
@@ -19,19 +27,88 @@ gulp.task("style", function() {
         "last 2 Opera versions",
         "last 2 Edge versions"
       ]})
+      /*,
+      mqpacker({
+        sort: true
+      })
+      */
     ]))
-    .pipe(gulp.dest("css"))
-    .pipe(server.reload({stream: true}));
+    .pipe(gulp.dest("build/css"))
+    .pipe(minify())
+    .pipe(rename("style.min.css"))
+    .pipe(gulp.dest("build/css"));
 });
 
-gulp.task("serve", ["style"], function() {
+//Минифицировать изображения
+gulp.task("images", function() {
+  return gulp.src("img/*.*")
+    .pipe(imagemin({
+      optimizationLevel: 3,
+      progressive: true
+    }))
+    .pipe(gulp.dest("build/img"));
+});
+
+//Минифицировать и скопировать js-файлы
+gulp.task("js", function() {
+  return gulp.src("js/*.js")
+    .pipe(plumber())
+    .pipe(gulp.dest("build/js"))
+    .pipe(rename({
+      suffix:".min",
+      extname: "js"
+    }))
+    .pipe(gulp.dest("build/js"));
+});
+
+//Скопировать щрифты
+gulp.task("copy:fonts", function() {
+  return gulp.src("fonts/**/*.{woff,woff2}")
+    .pipe(gulp.dest("build/fonts"))
+});
+
+//Скопировать img
+gulp.task("copy:img", function() {
+  return gulp.src("img/**/*")
+    .pipe(gulp.dest("build/img"));
+});
+
+//Скопировать html-файлы
+gulp.task("copy:html", function() {
+  return gulp.src("*.html")
+    .pipe(gulp.dest("build"));
+});
+
+//Очистить папку build при повторной сборке
+gulp.task("clean", function () {
+  return gulp.src("build", {read: false})
+    .pipe(clean({force: true}))
+});
+
+//Собрать проект
+gulp.task("build", function() {
+  runSequence(
+    "clean",
+    "copy:fonts",
+    "copy:html",
+    "copy:img",
+    "style",
+    //"images",
+    "js"
+  );
+});
+
+gulp.task("serve", function() {
   server.init({
-    server: ".",
+    server: "build",
     notify: false,
     open: true,
     ui: false
   });
 
-  gulp.watch("sass/**/*.{scss,sass}", ["style"]);
-  gulp.watch("*.html").on("change", server.reload);
+  gulp.watch("js/*.js", ["js"]);
+  gulp.watch("sass/*.scss", ["style"]);
+  gulp.watch("*.html", ["copy:html"]);
 });
+
+gulp.task('default', ['build']);
